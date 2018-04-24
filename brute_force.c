2c8c5297693bd22aa1fe27b928fcf0937bd38bc3
Marcus Daly <mrd109@case.edu>
2018-04-23 21:38:15 -0400

    Add Rest of original files

--- brute_force.c
+++ brute_force.c
@@ -7,53 +7,11 @@
 //global output filename
 FILE* output;
 
-int threadBody(unsigned char* ciphertext, int ciphertextLength, unsigned char* decryptedText){
-    //
-    return 0;
-}
-
 int decrypt(unsigned char* ciphertext, int ciphertextLength, unsigned char* key,
-            unsigned char* initvector, unsigned char* plaintext){
-    EVP_CIPHER_CTX *ctx;
-    
-    int length;
-    
-    int plaintextLength;
-    
-    // Create and initialize the context
-    if(!(ctx = EVP_CIPHER_CTX_new())) {
-        printf("failed to initialize decryption context\n");
-        return -1;
-    }
-    
-    //initialize the decryption operation
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, initvector)){
-        printf("decryption scheme initialization has failed\n");
-        return -1;
-    }
-    
-    // do the actual decryption of the ciphertext
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &length, ciphertext, ciphertextLength)){
-        printf("decryption of the ciphertext has failed");
-        return -1;
-    }
-    plaintextLength = length;
-    printf("%s",plaintext);
-    
-    //finalization of decryption - may add more plaintext
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + length, &length)) {
-        printf("decryption finalization has failed. OpenSSL error: %s\n",
-               ERR_error_string(ERR_get_error(), NULL));
-        return -1;
-    }
-    plaintextLength += length;
-    
-    // Clean up decryption environment
-    EVP_CIPHER_CTX_free(ctx);
-    
-    return plaintextLength;
-}
-
+            unsigned char* initvector, unsigned char* plaintext);
+double countProbEnglWords(char str[]);
+int search(char word[]);
+void sorting();
 
 int main (void){
     
@@ -115,4 +73,121 @@ int main (void){
     }
     return 0;
 }
+
+int decrypt(unsigned char* ciphertext, int ciphertextLength, unsigned char* key,
+            unsigned char* initvector, unsigned char* plaintext){
+    EVP_CIPHER_CTX *ctx;
+    
+    int length;
+    
+    int plaintextLength;
+    
+    // Create and initialize the context
+    if(!(ctx = EVP_CIPHER_CTX_new())) {
+        printf("failed to initialize decryption context\n");
+        return -1;
+    }
+    
+    //initialize the decryption operation
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, initvector)){
+        printf("decryption scheme initialization has failed\n");
+        return -1;
+    }
+    
+    // do the actual decryption of the ciphertext
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &length, ciphertext, ciphertextLength)){
+        printf("decryption of the ciphertext has failed");
+        return -1;
+    }
+    plaintextLength = length;
+    printf("%s",plaintext);
+    
+    //finalization of decryption - may add more plaintext
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + length, &length)) {
+        printf("decryption finalization has failed. OpenSSL error: %s\n",
+               ERR_error_string(ERR_get_error(), NULL));
+        return -1;
+    }
+    plaintextLength += length;
+    
+    // Clean up decryption environment
+    EVP_CIPHER_CTX_free(ctx);
+    
+    return plaintextLength;
+}
+
+
+double countProbEnglWords(char str[]) {
+    const int MAX_WORDS = 50;
+    const int MAX_CHARS = 100;
+    char c[MAX_WORDS][MAX_CHARS];
+    int possible_words = 0;
+    double words = 0;
+    const char delim[2] = " ";
+    
+    char *token;
+    char *rest = str;
+    
+    while((token = strtok_r(rest, " ", &rest))) {
+        strcpy(c[possible_words], token);
+        token = strtok(str, " ");
+        if (search(c[possible_words])) {
+            printf("A WORD!: %s\n", c[possible_words]);
+            words++;
+        }
+        possible_words++;
+    }
+    return words / possible_words;
+}
+
+int search(char word[]) {
+    // To Store Current Word
+    char c[100];
+    
+    // Points to current location in words.txt
+    FILE *fptr;
+    
+    // New-Line-delimited .txt Dictionary
+    if ((fptr = fopen("words.txt", "r")) == NULL)
+    {
+        printf("Error! opening file\n");
+        // Program exits if file pointer returns NULL.
+        exit(1);
+    }
+    
+    int max = 500000;
+    char ccs[max][100];
+    // Reads text until newline
+    int i = 0;
+    while (fscanf(fptr, "%s", c) != EOF && i < max) {
+        if (strcasecmp(word, c) == 0) {
+            fclose(fptr);
+            return 1;
+        }
+        //Copy c contents to a new string
+        if (i == max - 1)
+            printf("Data from the file: %s\n", c);
+        i++;
+    }
+    fclose(fptr);
+    return 0;
+}
+
+void sorting() {
+    const int SIZE = 50;
+    int ints[SIZE];
+    int j;
+    for (j = 0; j < SIZE; j++) {
+        ints[j] = rand();
+    }
+    
+    heapsort(ints, SIZE, sizeof(int), (int(*)(const void*, const void*))comparator);
+    
+    int i;
+    
+    for (i = 0; i < SIZE; i++) {
+        printf("%d\n", ints[i]);
+    }
+}
+
 
