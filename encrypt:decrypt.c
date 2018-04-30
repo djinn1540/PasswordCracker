4ebb6cea905b8ffe8093bfbd83d79d55a6d4a2e9
Greg Flynn <gregflynn@Greg-Flynns-MacBook-Pro.local>
2018-04-30 00:08:40 -0400

    moved enc file opening to inside loop to get correct reading of file

--- encrypt:decrypt.c
+++ encrypt:decrypt.c
@@ -5,6 +5,7 @@
 #include <openssl/rand.h>
 #include <string.h>
 #include <stdio.h>
+//#include <omp.h>
 
 typedef struct _cipher_params_t{
     unsigned char *key;
@@ -12,6 +13,18 @@ typedef struct _cipher_params_t{
     unsigned int encrypt;
     const EVP_CIPHER *cipher_type;
 }cipher_params_t;
+
+struct probableMessage{
+    double probability;
+    char* message;
+};
+
+double countProbEnglWords(char str[]);
+int search(char word[]);
+void sorting(struct probableMessage* pm, int SIZE, FILE* out);
+int comparator(const void* p, const void* q);
+
+struct probableMessage* results;
 
 /* 32 byte key (256 bit key) */
 #define AES_256_KEY_SIZE 32
@@ -28,6 +41,8 @@ void insertNum(int num, char* str, int strlen){
         else{
             digit = num % 10;
             num = num /10;
+            printf("your index is %d\n", num);
+            fflush(stdout);
             
             *(str+i) = digit + '0';
         }
@@ -80,6 +95,7 @@ void file_encrypt_decrypt(cipher_params_t *params, FILE *ifp, FILE *ofp){
             fprintf(stderr, "ERROR: EVP_CipherUpdate failed. OpenSSL error: %s\n",
                     ERR_error_string(ERR_get_error(), NULL));
             EVP_CIPHER_CTX_cleanup(ctx);
+            out_buf[0] = '\0';
             //cleanup(params, ifp, ofp, ERR_EVP_CIPHER_UPDATE);
         }
         fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
@@ -97,13 +113,17 @@ void file_encrypt_decrypt(cipher_params_t *params, FILE *ifp, FILE *ofp){
     /* Now cipher the final block and write it out to file */
     if(!EVP_CipherFinal_ex(ctx, out_buf, &out_len)){
         //bad decrypt - throws an error if the decrypt func finds something undexpected -> made our heapsort on the decrpyt results almost useless
+        out_buf[0] = '\0';
         EVP_CIPHER_CTX_cleanup(ctx);
         //cleanup(params, ifp, ofp, ERR_EVP_CIPHER_FINAL);
     }
-    else{
-        //this is where we write to the buffer we want to tokenize
-        fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
+        //this is where we write to the file that is read into the buffer we want to tokenize
+    fwrite(out_buf, sizeof(unsigned char), out_len, ofp);
+    if(params->encrypt == 0){
+        printf("%s",out_buf);
+        fflush(stdout);
     }
+    
     
     if (ferror(ofp)) {
         fprintf(stderr, "ERROR: fwrite error: %s\n", strerror(errno));
@@ -115,7 +135,7 @@ void file_encrypt_decrypt(cipher_params_t *params, FILE *ifp, FILE *ofp){
 
 
 int main(int argc, char *argv[]) {
-    FILE *f_input, *f_enc, *f_dec;
+    FILE *f_input, *f_enc, *f_dec,*outfile;
     
     /* Make sure user provides the input file */
     if (argc != 2) {
@@ -187,43 +207,82 @@ int main(int argc, char *argv[]) {
     params->encrypt = 0;
     
     
+    
+    int const KEYSPACESIZE = 10;
+    results = (struct probableMessage*)malloc(sizeof(struct probableMessage)*KEYSPACESIZE);
+    
+    fflush(stdout);
+    
     //here is where we insert our loop for key iteration
-    for(int i = 0; i < 10; i++){
+//#pragma omp parallel for
+    for(int i = 0; i < KEYSPACESIZE; i++){
+        
+        /* Open the encrypted file for reading in binary ("rb" mode) */
+        FILE* f_input2 = fopen("encrypted_file", "rb");
+        if (!f_input2) {
+            /* Unable to open file for reading */
+            fprintf(stderr, "ERROR: fopen error: %s\n", strerror(errno));
+            return errno;
+        }
     
         //make a guessed key based on the iteration index of the loop
         unsigned char* guessedKey[AES_256_KEY_SIZE];
         char* temp;
         temp = (char*)malloc(sizeof(char)*AES_256_KEY_SIZE);
-        insertNum(i, temp, AES_256_KEY_SIZE); // the int is the number in the key that will be prepended with 0's
+        insertNum(i, temp, AES_256_KEY_SIZE); // the int is the number in the key that will be prepended with 0's todo change 1-> i
+       // printf("%s", temp);
+       // fflush(stdout);
+        printf("fools!");
+        fflush(stdout);
         memcpy(key, temp, AES_256_KEY_SIZE);
+        printf("%s", key);
+        fflush(stdout);
         free(temp);
         
         //alter params with the guessed key
         params->key = key;
         //keep the iv the same bc Kerckhoffs's principle say the attacker can know all except the key
     
-        /* Open the encrypted file for reading in binary ("rb" mode) */
-        f_input = fopen("encrypted_file", "rb");
-        if (!f_input) {
-            /* Unable to open file for reading */
-            fprintf(stderr, "ERROR: fopen error: %s\n", strerror(errno));
-            return errno;
-        }
-    
-        /* Open and truncate file to zero length or create decrypted file for writing */
-        f_dec = fopen("decrypted_file", "wb");
-        if (!f_dec) {
+        
+        
+        char* decrypted_phrase;
+        char filename[36];
+        sprintf(filename, "decry%d", i);
+        printf("%s",filename);
+        fflush(stdout);
+        outfile = fopen(filename, "wb");
+        if (!outfile) {
             /* Unable to open file for writing */
             fprintf(stderr, "ERROR: fopen error: %s\n", strerror(errno));
             return errno;
         }
-    
+        
         /* Decrypt the given file */
-        file_encrypt_decrypt(params, f_input, stdout); //f_dec instead of stdout
+        file_encrypt_decrypt(params, f_input2, outfile); //f_dec instead of decrypted_phrase to print to file
+        //read_chars_from_file(decrypted_phrase, )
+        fclose(outfile);
+        
+        double percent = countProbEnglWords(decrypted_phrase);
+        struct probableMessage currentMessage;
+        currentMessage.message = decrypted_phrase;
+        currentMessage.probability = percent;
+        
+        results[i] = currentMessage;
+        fclose(f_input2);
     }
     
+    /* Open and truncate file to zero length or create decrypted file for writing */
+    f_dec = fopen("probable_decryptions", "wb");
+    if (!f_dec) {
+        /* Unable to open file for writing */
+        fprintf(stderr, "ERROR: fopen error: %s\n", strerror(errno));
+        return errno;
+    }
+    
+    sorting(results, KEYSPACESIZE, f_dec);
+    
     /* Close the open file descriptors */
-    fclose(f_input);
+    
     fclose(f_dec);
     
     /* Free the memory allocated to our structure */
@@ -231,3 +290,88 @@ int main(int argc, char *argv[]) {
     
     return 0;
 }
+
+double countProbEnglWords(char str[]) {
+    const int MAX_WORDS = 50;
+    const int MAX_CHARS = 100;
+    char c[MAX_WORDS][MAX_CHARS];
+    int possible_words = 0;
+    double words = 0;
+    const char delim[2] = " ";
+    
+    char *token;
+    char *rest = str;
+    
+    while((token = strtok_r(rest, " ", &rest))) {
+        strcpy(c[possible_words], token);
+        token = strtok(str, " ");
+        if (search(c[possible_words])) {
+            printf("A WORD!: %s\n", c[possible_words]);
+            words++;
+        }
+        possible_words++;
+    }
+    return words / possible_words;
+}
+
+int search(char word[]) {
+    // To Store Current Word
+    char c[100];
+    
+    // Points to current location in words.txt
+    FILE *fptr;
+    
+    // New-Line-delimited .txt Dictionary
+    if ((fptr = fopen("words.txt", "r")) == NULL)
+    {
+        printf("Error! opening file\n");
+        // Program exits if file pointer returns NULL.
+        exit(1);
+    }
+    
+    int max = 500000;
+    char ccs[max][100];
+    // Reads text until newline
+    int i = 0;
+    while (fscanf(fptr, "%s", c) != EOF && i < max) {
+        if (strcasecmp(word, c) == 0) {
+            fclose(fptr);
+            return 1;
+        }
+        //Copy c contents to a new string
+        if (i == max - 1)
+            printf("Data from the file: %s\n", c);
+        i++;
+    }
+    fclose(fptr);
+    return 0;
+}
+
+void sorting(struct probableMessage* pm, int SIZE, FILE* out) {
+    printf("About to sort!\n");
+    heapsort(pm, SIZE, sizeof(struct probableMessage), (int(*)(const void*, const void*))comparator);
+    printf("Just sorted!\n");
+    
+    int k;
+    for (k = 0; k < 5; k++) {
+        char * msg = "";
+        asprintf(&msg,"%d: %f:\n\t%s\n", k, pm[k].probability, pm[k].message);
+        printf("%s", msg);
+        
+        //output the probabilistically sorted messages to the output file
+        fputs(msg, out);
+    }
+    
+    
+    
+    fclose(out);
+}
+
+int comparator(const void* p, const void* q){
+    
+    struct probableMessage a = *((struct probableMessage*) p);
+    struct probableMessage b = *((struct probableMessage*) q);
+    
+    return ((int)((b.probability)*100)) - ((int)((a.probability)*100));
+}
+
